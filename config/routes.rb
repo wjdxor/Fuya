15fda8fe8e4bd6b69ae7036c195930d8d974054e
EC2 Default User <ec2-user@ip-172-31-34-1.us-east-2.compute.internal>
2018-08-14 11:07:19 +0000

    JT

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   resources :markets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'markets#index'
+  root 'markets#index' 
 end
